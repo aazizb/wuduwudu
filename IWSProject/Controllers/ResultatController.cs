c0dd85c2e66c8904d3dd6485ff8367599a66a32b
doghol daru <aazizb@hotmail.com>
2017-07-12 17:50:42 +0200

    Bilan-balance UI

---
+++ IWSProject/Controllers/ResultatController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IWSProject.Models;
+
+
+namespace IWSProject.Controllers
+{
+    public class ResultatController : Controller
+    {
+        // GET: Resultat
+        public ActionResult Index(string account, string start, string end, string company)
+        {
+            return View(IWSLookUp.GetResultat("9900","201701","201712","1000"));
+        }
+
+        // GET: Resultat
+        public ActionResult ResultatPartialView(string account, string start, string end, string company)
+        {
+            var resultat = IWSLookUp.GetResultat("9900", "201701", "201712", "1000");
+            var model = new List<ResultsViewModel>();
+            foreach (ResultsViewModel item in resultat)
+            {
+                model.Add(item);
+            }
+            return PartialView(model);
+        }
+
+    }
+}
